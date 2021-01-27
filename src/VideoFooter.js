10de1b5abd0dfe0164ba1f3ff77b239f70d4c3a5
Sune Carelse <sune.carelse@takealot.com>
2021-01-27 23:43:10 +0200

    created footer and sidebar components

---
+++ src/VideoFooter.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './VideoFooter.css';
+
+function VideoFooter() {
+  return (
+    <div className="videoFooter">
+      
+    </div>
+  );
+}
+
+export default VideoFooter;
