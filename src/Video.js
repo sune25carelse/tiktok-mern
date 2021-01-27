c0846f686fb301637c0cd1851a8dda6237c57230
Sune Carelse <sune.carelse@takealot.com>
2021-01-27 23:37:32 +0200

    setup play and pause function

--- src/Video.js
+++ src/Video.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import './Video.css';
 
 function Video() {
+  const [playing, setPlyaing] = useState(false);
+  const videoRef = useRef(null);
+
+  const handleVideoPress = () => {
+    if (playing) {
+      videoRef.current.pause();
+      setPlyaing(false)
+    } else {
+      videoRef.current.play();
+      setPlyaing(true);
+    }
+  };
+
   return (
     <div className="video">
-      <video src=" "></video>
-      
+      <video
+      onClick={handleVideoPress}
+        className="video__player"
+        loop
+        ref={videoRef}
+        src="https://static.videezy.com/system/resources/previews/000/005/575/original/matrix.mp4"></video>
+
       {/* VideoFooter */}
       {/* VideoSidebar */}
     </div>
-  );
+  )
 }
 
 export default Video;
