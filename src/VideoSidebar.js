b66431bca764fae293e4519bef6e6ee75e8c90bc
Sune Carelse <sune.carelse@takealot.com>
2021-01-28 21:26:33 +0200

    created styled sidebuttons share like

--- src/VideoSidebar.js
+++ src/VideoSidebar.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import './VideoSidebar.css';
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import MessageIcon from "@material-ui/icons/Message";
+import ShareIcon from "@material-ui/icons/Share";
 
 function VideoSidebar() {
   return (
     <div className="videoSidebar">
-      
+      <div className="videoSidebar__button">
+        <FavoriteIcon fontSize="large" />
+
+      </div>
+      <div className="videoSidebar__button">
+        <MessageIcon fontsize=""large />
+        
+      </div>
+      <div className="videoSidebar__button">
+        <ShareIcon fontSize="large" />
+
+      </div>
     </div>
   );
 }
