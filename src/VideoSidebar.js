03af19fd44993588fa3c775bef6ae9c78720cd53
Sune Carelse <sune.carelse@takealot.com>
2021-01-28 21:45:40 +0200

    created Fav share  icons  /  onClick function

--- src/VideoSidebar.js
+++ src/VideoSidebar.js
@@ -1,23 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './VideoSidebar.css';
 import FavoriteIcon from "@material-ui/icons/Favorite";
 import MessageIcon from "@material-ui/icons/Message";
 import ShareIcon from "@material-ui/icons/Share";
+import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
 
 function VideoSidebar() {
+const [liked, setLiked] = useState(false)
+
   return (
     <div className="videoSidebar">
       <div className="videoSidebar__button">
-        <FavoriteIcon fontSize="large" />
-
+        {liked ? (
+          <FavoriteIcon 
+            fontSize="large" 
+            onClick={(e) => setLiked(false)}
+          />
+        ) : (
+          <FavoriteBorderIcon 
+            fontSize="large"
+            onClick={(e) => setLiked(true)}
+          />
+        )}
+          <p>100</p>
       </div>
       <div className="videoSidebar__button">
-        <MessageIcon fontsize=""large />
-        
+        <MessageIcon fontSize="large" />
+        <p>250</p>
       </div>
       <div className="videoSidebar__button">
         <ShareIcon fontSize="large" />
-
+        <p>12</p>
       </div>
     </div>
   );
